aba273ea68d877c63adb5a80ff50b5d75d281218
eigoen <kaddalaijoo@gmail.com>
2017-01-25 03:20:57 +0800

    Update scriptsitemapbaru.js

--- scriptsitemapbaru.js
+++ scriptsitemapbaru.js
@@ -1,4 +1,4 @@
-var cat_numb=7;cat_pre='Prev';cat_nex='Next';
+var cat_home='http://www.bloggersstand.com' cat_numb=7;cat_pre='Prev';cat_nex='Next';
 var cat_name;var cat_start;var cat_class;
 function show_post2(a){
   var tt=a.feed.openSearch$totalResults.$t;dw='';dw+='<ul>';
@@ -35,4 +35,4 @@ function show_post2(a){
     dw+='<a href="" onclick="show_post(\''+cat[i].term+'\',\'1\',\''+i+'\');return false" title="';dw+=cat[i].term;dw+='">';
     dw+=cat[i].term;dw+='</a>';dw+='</li>'
   }dw+='</ul>';document.getElementById('show-cat').innerHTML=dw
-}
+}document.write('<script type="text/javascript" src="'+cat_home+'/feeds/posts/default?alt=json-in-script&max-results=0&callback=show_cat"><\/script>');
